2fa3b43865bf5eeb2ffe5a19bc4156bd5881e930
David Barraud <davidbarraud@gmail.com>
2023-06-25 17:46:35 -0300

    con parte del sigIn

--- netflixg2/src/pages/_app.js
+++ netflixg2/src/pages/_app.js
@@ -2,6 +2,7 @@ import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import { SSRProvider } from '@react-aria/ssr'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css'
+import { AppFirebase } from '@/firebase/InitConfig';
 
 export default function App({ Component, pageProps }) {
   return (
